c478e344b9612d7ac910bdf795d629558d6b2c81
Taylor, Raymond <raymond.taylor@intel.com>
2026-06-25 14:54:23 -0700

    demonstrate using the debug diagnostics layer

--- cmake-clang/src/main.cpp
+++ cmake-clang/src/main.cpp
@@ -1,36 +1,55 @@
 #include <IntelPresentMon/PresentMonAPIWrapper/PresentMonAPIWrapper.h>
+#include <IntelPresentMon/PresentMonAPIWrapper/DiagnosticHandler.h>
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <cstdio>
 
 using namespace std::literals;
 
 int main(int argc, const char** argv)
 {
-	// open a session with the service
-	pmapi::Session session;
+	const int targetPid = (argc > 1) ? std::stoi(argv[1]) : 12345;
 
-	// register a query for FPS and temperature (hardcoding gpu device id for most common case)
-	PM_QUERY_ELEMENT qels[] = {
-		{.metric = PM_METRIC_PRESENTED_FPS, .stat = PM_STAT_AVG},
-		{.metric = PM_METRIC_GPU_TEMPERATURE, .stat = PM_STAT_AVG, .deviceId = 1},
-	};
-	auto dq = session.RegisterDynamicQuery(qels);
-	// data blob for receiving query results conveniently allocated using the registered query object
-	auto blobs = dq.MakeBlobContainer(1);
+	try {
+		pmapi::Session session;
 
-	// begin tracking target application
-	auto tc = session.TrackProcess(std::stoi(argv[1]));
+		const int diagnosticOutputs =
+			PM_DIAGNOSTIC_OUTPUT_FLAGS_STDERR | PM_DIAGNOSTIC_OUTPUT_FLAGS_DEBUGGER;
+		pmapi::DiagnosticHandler diagnostics{
+			PM_DIAGNOSTIC_LEVEL_VERBOSE,
+			diagnosticOutputs,
+		};
 
-	while (true) {
-		// poll the tracked application using our query, result goes into our data blob
-		dq.Poll(tc, blobs);
-		// use the offsets in the query element list (written during registration) to access results
-		const auto pBytes = blobs.GetFirst();
-		std::cout << "fps:" << *reinterpret_cast<double*>(&pBytes[qels[0].dataOffset])
-			<< " temp:" << *reinterpret_cast<double*>(&pBytes[qels[1].dataOffset]) << std::endl;
-		// poll every 50ms
-		std::this_thread::sleep_for(50ms);
+		PM_QUERY_ELEMENT qels[] = {
+			{.metric = PM_METRIC_PRESENTED_FPS, .stat = PM_STAT_AVG},
+			{.metric = PM_METRIC_GPU_TEMPERATURE, .stat = PM_STAT_AVG, .deviceId = 1},
+		};
+		auto dq = session.RegisterDynamicQuery(qels);
+		auto blobs = dq.MakeBlobContainer(1);
+
+		try {
+			auto tc = session.TrackProcess(targetPid);
+			for (int i = 0; i < 40; ++i) {
+				try {
+					dq.Poll(tc, blobs);
+					const auto pBytes = blobs.GetFirst();
+					std::cout << "fps:" << *reinterpret_cast<double*>(&pBytes[qels[0].dataOffset])
+						<< " temp:" << *reinterpret_cast<double*>(&pBytes[qels[1].dataOffset]) << std::endl;
+				}
+				catch (const pmapi::ApiErrorException& ex) {
+					std::cerr << "poll error: " << ex.what() << std::endl;
+				}
+				std::this_thread::sleep_for(50ms);
+			}
+		}
+		catch (const pmapi::ApiErrorException& ex) {
+			std::cerr << "track error: " << ex.what() << std::endl;
+		}
+	}
+	catch (const pmapi::Exception& ex) {
+		std::fprintf(stderr, "presentmon client error: %s\n", ex.what());
+		return 1;
 	}
 
 	return 0;
